7f25ff120f5d067cd71ea1fd669fdf52eb980948
Cetins <69481095+Cetins@users.noreply.github.com>
2022-06-09 23:12:54 +0100

    booking repository added, tested in psql, working nicely

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ from models.member import Member
 from models.course import Course
 import repositories.member_repository as member_repository
 import repositories.course_repository as course_repository
+import repositories.booking_repository as booking_repository
 
 member_1 = Member("Charlie", False, True)
 member_repository.save(member_1)
@@ -25,6 +26,8 @@ course_repository.save(course_2)
 course_3 = Course("Kick Box", datetime.datetime(2022, 8, 7, 15, 00, 0), 5, True)
 course_repository.save(course_3)
 
+booking_repository.save(member_2, course_3)
+booking_repository.save(member_1, course_2)
 
 
 pdb.set_trace()
