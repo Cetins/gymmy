e5f6bced12030c5f769a4fa72facd465465b1530
Cetins <69481095+Cetins@users.noreply.github.com>
2022-06-10 11:40:03 +0100

    /members page route added

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 from flask import Flask, render_template
 
+from controllers.member_controller import members_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(members_blueprint)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
