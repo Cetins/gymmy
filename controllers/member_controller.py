282d725b27b9bec74016379bce8f75a86ebf1b49
Cetins <69481095+Cetins@users.noreply.github.com>
2022-06-10 15:35:32 +0100

    add new member comppleted, tested, working fine

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -14,3 +14,17 @@ def members():
 def show(id):
     member = member_repository.select(id)
     return render_template("members/show.html", member=member)
+
+@members_blueprint.route("/members/new")
+def new():
+    return render_template("members/new.html")
+
+@members_blueprint.route("/members", methods=["POST"])
+def add_member():
+    name = request.form['member_name']
+    premium = request.form['premium']
+    active = request.form['active']
+    new_member = Member(name, premium, active)
+    member_repository.save(new_member)
+    
+    return redirect("/members")
