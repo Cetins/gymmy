83ef597eaca70c964d85d1505bc621d4e2525486
Cetins <69481095+Cetins@users.noreply.github.com>
2022-06-13 01:05:17 +0100

    adding/deleting booking connected with course capacity

--- repositories/booking_repository.py
+++ repositories/booking_repository.py
@@ -3,12 +3,16 @@ from db.run_sql import run_sql
 from models.booking import Booking
 from models.member import Member
 from models.course import Course
+import repositories.course_repository as course_repository
 
 def save(booking):
     sql = "INSERT INTO bookings (member_id, course_id) VALUES (%s, %s) RETURNING id"
     values = [booking.member.id, booking.course.id]
     results = run_sql(sql, values)
     id = results[0]['id']
+    course = course_repository.select(booking.course.id)
+    course.capacity -=1
+    course_repository.update(course)
     booking.id = id
 
 def select_all():
@@ -34,9 +38,14 @@ def update(booking):
     run_sql(sql, values)
     
 def delete(id):
+    booking = select(id)
+    course = course_repository.select(booking['course_id'])
+    course.capacity +=1
+    course_repository.update(course)
     sql = "DELETE FROM bookings WHERE id=%s"
     values = [id]
     run_sql(sql, values)
+    
     
 def delete_all():
     sql = "DELETE FROM bookings"
